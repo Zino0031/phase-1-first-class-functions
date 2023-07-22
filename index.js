d19aa71fd85141c958790c384fd2f6dc42753205
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-07-22 20:13:52 +0200

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function receivesAFunction (callback){
+   callback();
+}
+
+receivesAFunction();
+
+function returnsANamedFunction (){
+   function Named(){
+
+   }
+ return Named;
+}
+
+
+
+ function returnsAnAnonymousFunction(){
+    return function(){
+      
+    };
+ }
